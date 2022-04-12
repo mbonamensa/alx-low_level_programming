c105f884d3c601a333cae9c6809554704453ec1f
Maame <mbonamensa@gmail.com>
2022-04-12 11:38:36 -0700

    Task 3 of 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,20 +7,21 @@
  */
 
 int main(void)
+
 {
 	char lowerCase;
 	char upperCase;
+
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+        {
+                putchar(lowerCase);
+        }
 
 	for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
 	{
 		putchar(upperCase);
 	}
 
-	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
-	{
-		putchar(lowerCase);
-	}
-
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
