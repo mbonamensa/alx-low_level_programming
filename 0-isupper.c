621c3ee3e4c6a72bc945560939fcbb003ea91592
Maame <mbonamensa@gmail.com>
2022-04-15 16:27:24 -0700

    Task 0

--- 0-isupper.c
+++ 0-isupper.c
@@ -2,10 +2,18 @@
 
 /**
  * _isupper - checks for uppercase character
- * @letter: the character to be tracked
- * Return: 1 if c is uppercase, 0 otherwise
+ * @c: print int
+ * Return: Always 0
  */
-int _isupper(int letter)
+int _isupper(int c)
 {
-	return (letter >= 'A' && letter <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
 }
