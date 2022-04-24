6193d40b36910152533379f3048318abff26588a
Maame <mbonamensa@gmail.com>
2022-04-23 17:20:59 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,24 @@
 #include "main.h"
-
 /**
-* _strcmp - compares two strings
-* @s1: A pointer to a character that will be updated
-* @s2: A pointer to a character that will also be updated
-* 
-* Return: int
-*/
+ *_strcmp - compares two strings
+ *@s1: A pointer to a character that will be updated
+ *@s2: A pointer to a character that will also be updated
+ *Return: dest
+ */
 
 int _strcmp(char *s1, char *s2)
+
 {
-	int i = 0, cmp = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && cmp == 0)
-	{
-		cmp = s1[i] - s2[i];
-		i++;
-	}
+char *str_one = s1;
+char *str_two = s2;
 
-	return (cmp);
+
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+{
+str_one++;
+str_two++;
+}
+
+return (*str_one - *str_two);
 }
