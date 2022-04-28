76f1fc05cef70b519b42c75eb18733e2e464746b
Maame <mbonamensa@gmail.com>
2022-04-28 08:42:45 -0700

    Task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a new line
+ *
+ * Return: 0 
+ */
+
+void _puts_recursion(char *s);
+
+int main() {
+_puts_recursion(*s);
+return (0);
+}
+
+_puts_recursion() {
+	_putchar("This is my string %s\n", *s);
+	return (*s);
+}
