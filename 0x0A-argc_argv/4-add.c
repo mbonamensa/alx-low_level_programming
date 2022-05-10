500db6b7ec4a2f84a2198b59cc7946718409534a
Maame <mbonamensa@gmail.com>
2022-05-10 11:57:21 -0700

    Update Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
+
 
 /**
- * check_num - check - string there are digit
- * @str: array str
+ * _isnumber - checks if string is a number
+ * @s: string
  *
- * Return; Always 0 (Success)
+ * Return: On success 1.
+ * If not a number, 0 is returned.
  */
-
-int check_num(char *str)
-
+int _isnumber(char *s)
 {
-/*Declaring variables*/
-unsigned int count;
+	int i, check, d;
 
-count = 0;
-while (count < strlen(str)) /*count string*/
-
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
+	i = 0, d = 0, check = 1;
+	if (*s == '-')
+		i++;
+	for (; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		if (d == 0)
+		{
+			check = 0;
+			break;
+		}
+	}
+	return (check);
 }
+/**
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int j, ex, coins, cents, d;
+	int c[5] = {25, 10, 5, 2, 1};
 
-count++;
-}
-return (1);
+	ex = 1, j = 0, coins = 0;
+	if (argc == 2)
+	{
+		if (_isnumber(argv[1]))
+		{
+			ex = 0, cents = atoi(argv[1]);
+			if (cents >= 0)
+			{
+				while (cents != 0)
+				{
+					d = cents / c[j];
+					if (d == 0)
+					{
+						j++;
+					}
+					else
+					{
+						coins += d;
+						cents -= (d * c[j]);
+					}
+				}
+			}
+		}
+	}
+	if (ex == 0)
+		printf("%i\n", coins);
+	else
+		printf("%s\n", "Error");
+	return (ex);
 }
